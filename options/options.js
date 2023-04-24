7771100fc074393e19e93d1c5e077f1bd7361d66
Crowdbotics <team@crowdbotics.com>
2023-04-24 13:56:59 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "black-meadow-40164",
+  url: "https://black-meadow-40164.botics.co",
+  api: "https://black-meadow-40164.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
